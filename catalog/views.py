8d292d98d8e58e7d23850e86159b29a3257cbef6
Hristyaa <shhs200799@gmail.com>
2025-10-17 16:58:51 +0900

    views(home, contacts)

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+
+        return HttpResponse(f'Спасибо, {name}! Сообщение: "{message}" получено.')
+    return render(request, 'catalog/contacts.html')
