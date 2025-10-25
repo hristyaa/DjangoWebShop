9beabdb109f405c928233d3d403157f24ebc8f86
Hristyaa <shhs200799@gmail.com>
2025-10-25 19:19:15 +0900

    add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', verbose_name='Изображения', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name']
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -10,3 +13,6 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
