4750c87e4659685ac6b9ac5687585859dee038fc
Hristyaa <shhs200799@gmail.com>
2025-10-25 19:31:57 +0900

    add migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,32 +1,69 @@
 from django.db import models
 
+
 # Create your models here.
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование"
+    )
+    description = models.TextField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='images/', verbose_name='Изображения', null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование"
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Описание"
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Изображение",
+        null=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name="Категория"
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Цена"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата изменения"
+    )
 
     def __str__(self):
-        return f'{self.name} {self.description} {self.price}'
+        return f"{self.name} {self.description} {self.price}"
 
     class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-        ordering = ['name']
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = [
+            "name",
+            "price",
+        ]
