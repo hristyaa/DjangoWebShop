b94fcd8730c302ca6ebc633e30db2ee591135dc8
Hristyaa <shhs200799@gmail.com>
2025-11-01 23:07:49 +0900

    homework_24

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,21 +1,19 @@
-from django.urls import path
-
-from catalog.apps import CatalogConfig
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path
+
+from catalog.apps import CatalogConfig
 
 from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('products_list/', views.products_list, name='products_list'),
-    path('products/<int:pk>/', views.products_detail, name='products_detail'),
+    path("home/", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("products_list/", views.products_list, name="products_list"),
+    path("products/<int:pk>/", views.products_detail, name="products_detail"),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,40 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-from catalog.models import Product, Contact, Category
+from catalog.models import Contact, Product
 
 
 def home(request):
     products = Product.objects.all()
-    context = {'products': products}
-    latest_products = Product.objects.all().order_by('-created_at')[:5]
+    context = {"products": products}
+    latest_products = Product.objects.all().order_by("-created_at")[:5]
     print("Последние 5 созданных продуктов:")
     for product in latest_products:
-        print(f'{product.name} - {product.price} руб.')
-    return render(request, 'catalog/home.html', context)
+        print(f"{product.name} - {product.price} руб.")
+    return render(request, "catalog/home.html", context)
 
 
 def contacts(request):
     data = Contact.objects.all()
-    context = {'contacts': data}
+    context = {"contacts": data}
 
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        message = request.POST.get('message')
-
+    if request.method == "POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
 
         return HttpResponse(f'Спасибо, {name}! Сообщение: "{message}" получено.')
-    return render(request, 'catalog/contacts.html', context)
+    return render(request, "catalog/contacts.html", context)
 
 
 def products_list(request):
     products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/products_list.html', context)
-
+    context = {"products": products}
+    return render(request, "catalog/products_list.html", context)
 
 
 def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'catalog/products_detail.html', context)
+    context = {"product": product}
+    return render(request, "catalog/products_detail.html", context)
