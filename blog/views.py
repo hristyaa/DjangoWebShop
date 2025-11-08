d6674760b9b5bc052d943ddac4d2a8139a3ee0dd
Hristyaa <shhs200799@gmail.com>
2025-11-08 22:27:34 +0900

    send_mail if post.count_of_views == 100

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from blog.models import BlogPost
+from config import settings
 
 
 class BlogPostListView(ListView):
@@ -24,8 +26,25 @@ class BlogPostDetailView(DetailView):
     def get_object(self, queryset=None):
         self.post = super().get_object(queryset)
         self.post.count_of_views += 1
+
+        if self.post.count_of_views == 100:
+            self.send_to_email()
+
         self.post.save()
         return self.post
+
+    def send_to_email(self):
+        subject='Пост набрал 100 просмотров!'
+        message = f"Поздравляем! Ваш пост '{self.post.title}' набрал 100 просмотров!"
+
+        send_mail(
+                    subject,
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [settings.ADMIN_EMAIL],
+                    fail_silently=False,
+                )
+        print(f"Письмо на email отправлен для статьи: {self.post.title}")
 
 
 class BlogPostCreateView(CreateView):
