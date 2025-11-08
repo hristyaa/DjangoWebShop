c592a4aee44bc3df1a76102bf8d8e408428f3a98
Hristyaa <shhs200799@gmail.com>
2025-11-08 16:58:53 +0900

    home/

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
     path("products_list/", views.products_list, name="products_list"),
     path("products/<int:pk>/", views.products_detail, name="products_detail"),
