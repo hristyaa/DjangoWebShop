d1d2a3c4b4f4994cf074d2fbfea01ef0c8cc0170
Hristyaa <shhs200799@gmail.com>
2025-11-08 19:16:34 +0900

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from blog.models import BlogPost
 # Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "content",
+        "created_at",
+    )
+    search_fields = (
+        "id",
+        "title",
+        "created_at",
+    )
+
