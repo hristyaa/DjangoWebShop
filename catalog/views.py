7ac131305e1904831721ac7cd15ad450d7ac6c4b
Hristyaa <shhs200799@gmail.com>
2025-11-15 14:48:09 +0900

    product create, update, delete

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 # from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from catalog.models import Contact, Product
 
@@ -11,6 +13,26 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'description', 'image', 'category', 'price']
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:products_list')
 
 
 class ContactsTemplateView(TemplateView):
