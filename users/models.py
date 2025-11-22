243c04092f6f227c703cbc321db689a5d302594d
Hristyaa <shhs200799@gmail.com>
2025-11-22 14:18:23 +0900

    model of user, migrate+

--- users/models.py
+++ users/models.py
@@ -4,13 +4,17 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name="Аватар", help_text='Загрузите свой аватар')
+    phone_number = models.CharField(max_length=15, blank=True, null=True, verbose_name='Телефон', help_text='Введите номер телефона')
     country = models.CharField(max_length=50, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', ]
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return self.email
